4c59dd69d4a2c9fdf35fc98812c45b343a8f5d76
supun <supunmadushanka19980822@gmail.com>
2021-05-08 00:33:12 +0530

    test1

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+var nodemailer = require('nodemailer');
+
+var PORT = process.env.PORT || 3000
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', function(req, res) {
+    res.send('hello from server');
+})
+
+app.post('/test', function(req, res) {
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'supunmadushanka19980822@gmail.com',
+            pass: 'mynameissuperman#'
+        }
+    });
+
+    var mailOptions = {
+        from: 'supunmadushanka19980822@gmail.com',
+        to: 'supunmadushanka19980822@gmail.com',
+        subject: req.body.email + ' - ' + req.body.FullName,
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+            response.setHeader('Access-Control-Allow-Origin', '*')
+            response.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,DELETE,PUT')
+            response.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token')
+            res.status(200).send(info);
+        }
+    });
+})
+
+app.listen(PORT, function() {
+    console.log("Server running on localhost:" + PORT);
+});
