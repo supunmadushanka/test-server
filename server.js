0c65b6f0761155615327500dbd2cc9bb7a915118
supun <supunmadushanka19980822@gmail.com>
2021-05-09 17:50:32 +0530

    test7

--- server.js
+++ server.js
@@ -2,14 +2,34 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 var nodemailer = require('nodemailer');
+const sql = require('mssql');
 
 var PORT = process.env.PORT || 3000
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const sqlconfig = {
+    db: '*sql.DB',
+    user: 'supun',
+    password: 'Ranjani1970#',
+    server: 'mysport-codefreks.database.windows.net',
+    database: 'mysport',
+    port: 1433,
+    "options": {
+        "encrypt": true,
+        "enableArithAbort": true
+    }
+};
+
 app.get('/', function(req, res) {
-    res.send('hello from server');
+    let connection = sql.connect(sqlconfig, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send('db connected');
+        }
+    });
 })
 
 app.listen(PORT, function() {
